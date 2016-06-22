4f789f65c7a4426179697be00dfcd6809e7d77c0
Vincent Rupp Jr <vrupp@popsugar.com>
2016-06-22 09:42:02 -0700

    Roll and unroll the parameters

--- nn.py
+++ nn.py
@@ -15,7 +15,7 @@ def logistic_derivative(x):
     return logistic(x)*(1-logistic(x))
 
 class NeuralNetwork:
-    def __init__(self, activation='tanh'):
+    def __init__(self, test='./input/test.csv', train='./input/train.csv', num_labels=10, activation='logistic'):
         """
         :param layers: A list containing the number of units in each layer.
         Should be at least two values
@@ -23,29 +23,32 @@ class NeuralNetwork:
         "logistic" or "tanh"
         """
         
-        self.test = pd.read_csv('./input/test.csv')
-        self.train = pd.read_csv('./input/train.csv')
+        self.test = pd.read_csv(test)
+        self.train = pd.read_csv(train)
+        
+        self.input_layer_size = self.train.shape[1] - 1
+        self.hidden_layer_size = 25
+        self.num_labels = num_labels
         
         
         if activation == 'logistic':
             self.activation = logistic
-            self.activation_deriv = logistic_derivative
+            self.activation_derivative = logistic_derivative
         elif activation == 'tanh':
             self.activation = tanh
-            self.activation_deriv = tanh_derivative
+            self.activation_derivative = tanh_derivative
 
     def randInitializeWeights(self, L_in, L_out):
         """
         Randomly initialize the weights of a layer with L_in incoming
-        connections and L_out outgoing connections. Note that W is set
-        to a matrix of size(L_out, 1 + L_in) because the column row of
-        W handles the "bias" terms.
+        connections and L_out outgoing connections. Note the addition
+        of space (the +1s) for the "bias" terms.
         """
         
         epsilon_init = math.sqrt(6) / math.sqrt(L_in + L_out)
         return (np.random.random((L_out, 1 + L_in)) * 2 * epsilon_init) - epsilon_init
 
-    def nnCostFunction(self, nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, C):
+    def nnCostFunction(self, nn_params, X, y, lambda_):
         """
         Computes the cost and gradient of the neural network. The
         parameters for the neural network are "unrolled" into the vector
@@ -56,17 +59,18 @@ class NeuralNetwork:
         """
 
         # Initialize
+        
+        train_size = self.hidden_layer_size * (self.input_layer_size + 1)
+        Theta1 = np.reshape(nn_params[0:train_size],
+                               (self.hidden_layer_size, self.input_layer_size + 1))
+        Theta2 = np.reshape(nn_params[train_size + 1:],
+                               (self.num_labels, self.hidden_layer_size + 1))
 
-        Theta1 = np.reshape(nn_params[1:hidden_layer_size * (input_layer_size + 1)],
-                               (hidden_layer_size, (input_layer_size + 1)))
-        Theta2 = np.reshape(nn_params[1 + (hidden_layer_size * (input_layer_size + 1)):],
-                               (num_labels, (hidden_layer_size + 1)))
-
-#        m = np.shape(X)[0]
+        m = np.shape(X)[0]
 #
-#        J = 0
-#        Theta1_grad = np.zeros(np.shape(Theta1))
-#        Theta2_grad = np.zeros(np.shape(Theta2))
+        J = 0
+        Theta1_grad = np.zeros(np.shape(Theta1))
+        Theta2_grad = np.zeros(np.shape(Theta2))
 #
 #        # Feed forward from initial theta
 #        X = np.concatenate((np.ones([m, 1]), X), axis=1)
--- runner.py
+++ runner.py
@@ -7,14 +7,9 @@ Created on Tue Jun 21 08:48:46 2016
 
 import nn
 
-
-input_layer_size = 784 # 28x28 Input Images of Digits
-hidden_layer_size = 25 # 25 hidden units
-num_labels = 10        # 10 labels, from 1 to 10
-
 net = nn.NeuralNetwork()
 
-initial_Theta1 = net.randInitializeWeights(input_layer_size, hidden_layer_size)
-initial_Theta2 = net.randInitializeWeights(hidden_layer_size, num_labels)
+initial_Theta1 = net.randInitializeWeights(net.input_layer_size, net.hidden_layer_size)
+initial_Theta2 = net.randInitializeWeights(net.hidden_layer_size, net.num_labels)
 
-#initial_nn_params = 
+initial_nn_params = [item for sublist in initial_Theta1 for item in sublist] + [item for sublist in initial_Theta2 for item in sublist]
