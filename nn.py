9e95c9ff4db9138ed5b8612d1cc209f9a28967dd
vaerek <vrupp@popsugar.com>
2017-04-12 19:28:50 -0700

    Compute cost after each epoch

--- nn.py
+++ nn.py
@@ -25,6 +25,7 @@ class DigitClassifier:
                  weight_init="epsilon"):
 
         layers = layers or [25, 10]  # Default to one hidden layer with 25 units and one 10 unit output layer
+        # layers = layers or [256, 64, 10]
 
         if len(layers) < 2:
             raise TypeError('The layers arg should be a list containing at least two integers')
@@ -108,11 +109,6 @@ class DigitClassifier:
             a = np.array([np.insert(activations[-l - 1], 0, 1)]).T
             nabla[-l] = np.dot(delta[1:], a.T)
 
-        self.curr_cost = self.params["cost"](self._label(y), activations[-1])
-
-        if self.params["lamda"] > 0:
-            self.curr_cost += np.sum([np.sum(np.square(theta[:, 1:])) for theta in self.weights])
-
         return nabla
 
     def _label(self, y):
@@ -138,18 +134,19 @@ class DigitClassifier:
                 X = batch[:, 1:]
                 self._update_model(X, y)
 
-            self.params["alpha"] *= .90
+            if self.params["alpha"] < 0.01:
+                self.params["alpha"] *= 0.95
+            else:
+                self.params["alpha"] *= .90
 
-            # I've discovered empirically that steps smaller than this aren't that helpful in this data
-            if self.params["alpha"] < 0.005:
-                self.params["alpha"] = 0.005
+            self.curr_cost = self._compute_cost(train)
+
+            if not i % 5:
+                print('Current cost: {0}\n'.format(round(self.curr_cost, 3)))
 
             print('Epoch #{0} results:'.format(i + 1))
             print('\tTrain set accuracy: {0}%'.format(self.evaluate(train) * 100))
             print('\tTest set accuracy: {0}%\n'.format(self.evaluate(test) * 100))
-
-            if not i % 5:
-                print('Current cost: {0}\n'.format(round(self.curr_cost, 3)))
 
     def _update_model(self, X, y):
         nabla = [np.zeros(w.shape) for w in self.weights]
@@ -165,3 +162,13 @@ class DigitClassifier:
 
         for theta in self.weights:
             theta[:, 1:] = [w - ((alpha * lamda) / m) * w for w in theta[:, 1:]]
+
+    def _compute_cost(self, data):
+        cost = 0
+        m = len(data)
+        for row in data:
+            _, activations = self._feedforward(row[1:])
+            cost += self.params["cost"](self._label(row[0]), activations[-1]) / m
+
+        cost += np.sum([(self.params["lamda"] / (2 * m)) * np.sum(np.square(theta[:, 1:])) for theta in self.weights])
+        return cost
